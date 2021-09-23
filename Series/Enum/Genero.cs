4e704b46f50cb0ba3ef60774aa406cb579069251
andradealex09 <andradealex09@gmail.com>
2021-09-23 20:22:16 -0300

    Adicionar arquivos de projeto.

---
+++ Series/Enum/Genero.cs
@@ -0,0 +1,35 @@
+﻿namespace Series
+{
+
+    public enum Genero
+    {
+
+        Acao = 1,
+
+        Aventura = 2,
+
+        Comedia = 3,
+
+        Documentario = 4,
+
+        Drama = 5,
+
+        Espionagem = 6,
+
+        Faroeste = 7,
+
+        FiccaoCientifica = 8,
+
+        Musical = 9,
+
+        Romance = 10,
+
+        Suspense = 11,
+
+        Terror = 12
+
+
+
+    }
+ 
+}
---
+++ Series/SeriesRepositorio.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Series.Interface;
+
+namespace Series
+{
+    public class SeriesRepositorio : IRepositorio<Series>
+    {
+
+        private List<Series> listaSeries = new List<Series>();
+        public void Atualiza(int id, Series objeto)
+        {
+            listaSeries[id] = objeto;
+        }
+
+        public void Exclui(int id)
+        {
+            listaSeries[id].excluir();
+        }
+
+        public void Insere(Series objeto)
+        {
+            listaSeries.Add(objeto);
+        }
+
+        public List<Series> Lista()
+        {
+            return listaSeries;
+        }
+
+        public int ProximoId()
+        {
+            return listaSeries.Count;
+        }
+
+        public Series RetornaPorId(int id)
+        {
+            return listaSeries[id];
+        }
+    }
+}
